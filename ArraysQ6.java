8afe511b2f036b038896a699290347ed93fda204
Trang <quynhtranghbt@gmail.com>
2021-06-23 14:29:22 -0400

    init

---
+++ ArraysQ6.java
@@ -0,0 +1,20 @@
+
+ class QuestionA {	
+	public static String sort(String s) {
+		
+		return "moo";
+	}
+	
+	public static boolean permutation(String s, String t) {
+		return  false;	}	
+	
+	public static void main(String[] args) {
+		String[][] pairs = {{"apple", "papel"}, {"carrot", "tarroc"}, {"hello", "llloh"}};
+		for (String[] pair : pairs) {
+			String word1 = pair[0];
+			String word2 = pair[1];
+			boolean anagram = permutation(word1, word2);
+			System.out.println(word1 + ", " + word2 + ": " + anagram);
+		}
+	}
+}
---
+++ Template.java
@@ -0,0 +1,19 @@
+class Template {
+	public static void main(String[] args) {
+		String s = "";
+		String[] sa= {"moo","moo","moo"};
+		int i = 0;
+		int[] ia = {1000,1000,1000};
+		
+		
+		System.out.println(solution());
+		
+	}
+	
+	
+	public static String solution(){
+		
+		
+		return "mooo";
+	}
+}
